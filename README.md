eb2d9d606400e3e18ebc083e3efe8aae804c3771
Ryan Braun <ryan@thelyricla.com>
2020-07-31 00:33:38 -0700

    first 6 hours of building out functionality

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Quiz-Game
+
+July 31, 2020
+
+I started this quiz game by setting up the code for the countdown clock.  After setting up my HTML, I inserted the countdown div and began building my quiz game from there.  I started with the website alerting the user that they had 1 minute to complete the 5 questions, which are blank at the moment.  Once they click "OK" the timer begins. 
+
+**I am still working on the functionality of seconds being taken off of the timer.  
+
+/Users/rbraun04/Desktop/Screen Shot 2020-07-31 at 12.23.47 AM.png
+
+I then built out the question and answer functions using a variety of variables.  I followed an instructionary video on YouTube to map out the questions and answers (which are currently gibberish).  The video helped to clarify how to set the questions into a variable, how they are called through the functions, and how the answers are compared.
+
+I added "clearInterval" in the else section of the checkAnswer function as well as when the timer hits 0 in case the user does not select any questions.
+
+I also added a prompt in the scoreRender function that asks for the users initials.  Their response is then stored into a variable and stored into LocalStorage.  I may attempt to write a score card that compares all the stored scores and lists them in order.
+
+The next step will be styling and theming the quiz game and adding a few extra flares, perhaps alerts to trigger when time is getting low, or if answers are obviously wrong some sort of animation triggers.
+
+
---
+++ script.js
@@ -0,0 +1,173 @@
+var quiz = document.getElementById("quiz");
+var question = document.getElementById("question");
+var qImg = document.getElementById("qImg");
+var choiceA = document.getElementById("A");
+var choiceB = document.getElementById("B");
+var choiceC = document.getElementById("C");
+var scoreDiv = document.getElementById("scoreContainer");
+var timer = [];
+var scores = [];
+
+//question variables:
+let questions = [
+    {
+        question : "What does HTML stand for?",
+        imgSrc : "img/html.png",
+        choiceA : "Correct",
+        choiceB : "Wrong",
+        choiceC : "Wrong",
+        correct : "A"
+    },{
+        question : "What does CSS stand for?",
+        imgSrc : "img/css.png",
+        choiceA : "Wrong",
+        choiceB : "Correct",
+        choiceC : "Wrong",
+        correct : "B"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    },{
+        question : "What does JS stand for?",
+        imgSrc : "img/js.png",
+        choiceA : "Wrong",
+        choiceB : "Wrong",
+        choiceC : "Correct",
+        correct : "C"
+    }
+];
+
+// Alert to start the quiz:
+alert("You have 1 Minute to complete this Quiz!  For every wrong answer you will get deducted 5 seconds!! Choose well....!")
+
+
+let lastQuestion = questions.length - 1;
+let runningQuestion = 0;
+let count = 0;
+let score = 0;
+
+//start quiz
+
+
+// countdown function
+
+const startingMinutes = 1;
+let time = startingMinutes * 60
+
+const countdownEl = document.getElementById('countdown');
+
+setInterval(updateCountdown, 1000);
+
+
+renderQuestion();
+    
+
+function updateCountdown() {
+    const minutes = Math.floor(time/60);
+    let seconds = time % 60;
+
+    timer = seconds;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds; 
+
+    countdownEl.innerHTML = `${minutes}: ${seconds}`;
+    time--;
+    
+    
+     if (timer < 0) {
+       scoreRender();
+      clearInterval(startingMinutes);
+   }
+}
+//render questions function:
+
+function renderQuestion(){
+    let q = questions[runningQuestion];
+    
+    question.innerHTML = "<p>"+ q.question +"</p>";
+    choiceA.innerHTML = q.choiceA;
+    choiceB.innerHTML = q.choiceB;
+    choiceC.innerHTML = q.choiceC;
+}
+
+function startQuiz(){
+    start.style.display = "none";
+    renderQuestion();
+    quiz.style.display = "block";
+}
+
+function answerIsCorrect(){
+    
+}
+
+// answer is Wrong
+function answerIsWrong(){
+
+}
+
+function checkAnswer(answer){
+    if( answer == questions[runningQuestion].correct){
+        // answer is correct
+        score++;
+        // change progress color to green
+        answerIsCorrect();
+    }else{
+        // answer is wrong
+        // change progress color to red
+        answerIsWrong();
+    }
+    count = 0;
+    if(runningQuestion < lastQuestion){
+        runningQuestion++;
+        renderQuestion();
+    }
+    
+    else{
+        // end the quiz and show the score
+        clearInterval(startingMinutes);
+        scoreRender();
+    }
+}
+
+function scoreRender(){
+    
+    var playerInitials = prompt("Enter your intials!")
+    console.log(playerInitials)
+    
+
+    scoreDiv.style.display = "block";
+    
+    // calculate the amount of question percent answered by the user
+    const scorePerCent = Math.round(100 * score/questions.length);
+    
+    // choose the image based on the scorePerCent
+    let img = (scorePerCent >= 80) ? "img/5.png" :
+              (scorePerCent >= 60) ? "img/4.png" :
+              (scorePerCent >= 40) ? "img/3.png" :
+              (scorePerCent >= 20) ? "img/2.png" :
+              "img/1.png";
+    
+    scoreDiv.innerHTML = "<img src="+ img +">";
+    scoreDiv.innerHTML += "<p>"+"Thank you for playing " + playerInitials + " Your Score = " + scorePerCent + "%</p>";
+    
+    scores = localStorage.setItem(playerInitials, scorePerCent)
+
+}
